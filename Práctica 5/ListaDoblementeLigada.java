cea1cf6d3143c5e79a11cf267bb350e00c058a1e
arhernav <arhernav@ciencias.unam.mx>
2026-01-08 23:57:23 -0600

    Codigo añadido

--- Práctica 5/ListaDoblementeLigada.java
+++ Práctica 5/ListaDoblementeLigada.java
@@ -60,46 +60,112 @@ public class ListaDoblementeLigada<T> implements Lista<T> {
 
      @Override
      public void insertar(T elemento) throws IllegalArgumentException {
-          /*Aqui va tu código*/
+          if(elemento == null){throw new IllegalArgumentException("No se puede insertar null");}
+          Nodo newNode = new Nodo(elemento);
+          if(this.cabeza == null){
+               this.cabeza = this.rabo = newNode;
+               this.longitud += 1;
+          }else{
+               this.cabeza.anterior = newNode;
+               newNode.siguiente = this.cabeza;
+               this.cabeza = newNode; 
+               this.longitud += 1;
+          }
      }
 
      public void eliminar(T elemento){
-          /*Aqui va tu código*/
+          
      }
 
      @Override
      public boolean buscar(T elemento) {
-          /*Aqui va tu código*/
+          for(Nodo node: this){
+               if(node.elemento == elemento){
+                    return true;
+               }
+          }
+          return false;
      }
 
      @Override
      public void eliminar(int i) {
-          /*Aqui va tu código*/
+          Nodo toDelete = this.accederNodo(i);
+          toDelete.anterior.siguiente = toDelete.siguiente;
+          toDelete.siguiente.anterior = toDelete.anterior;
+          this.longitud--;
      }
 
      @Override
      public T acceder(int i) throws IllegalArgumentException {
-          /*Aqui va tu código*/     }
+          if(i < 0 || i>= this.longitud){throw new IllegalArgumentException("Este indice no se encuentra en la lista");}
+          return this.accederNodo(i).elemento;   
+     }
 
      public int devolverIndiceElemento(T elemento) throws IllegalArgumentException{
-          /*Aqui va tu código*/
+          Nodo supp = this.cabeza; 
+          for(int i = 0; i < this.longitud; i ++){
+               if(supp.elemento == elemento){
+                    return i;
+               }else{
+                    supp = supp.siguiente;
+               }
+          }
+          throw new IllegalArgumentException("El elemento no se encuentra en la lista");
      }
 
      public int darLongitud(){
-          /*Aqui va tu código*/
+          return this.longitud;
      }
 
      public void insertarFinal(T elemento) throws IllegalArgumentException {
-          /*Aqui va tu código*/
+          if(elemento == null){throw new IllegalArgumentException("No se puede insertar null");}
+          Nodo newNode = new Nodo;
+          if( this.cabeza == null){
+               this.cabeza = this.rabo = newNode;
+               this.longitud +=1;
+          }else{
+               this.rabo.siguiente = newNode;
+               newNode.anterior = this.rabo; 
+               this.longitud +=1;
+          }
      }
 
      public ListaDoblementeLigada<T> reversa(){
-          /*Aqui va tu código*/
+          ListaDoblementeLigada<T> reversa = new ListaDoblementeLigada<>();
+          if(this.cabeza == null){return reversa}
+          for(Nodo supp: this){
+               reversa.insertar(supp.elemento)
+          }
+          return reversa
      }
 
      
-     private Nodo accederNodo(int i) {
-          /*Aqui va tu código*/
+     private Nodo accederNodo(int i) throws IllegalArgumentException {
+          if(i < this.longitud || i >= this.longitud){throw new IllegalArgumentException("El indice no se encuentra en la lista");}
+          int middle = this.longitud/ 2;
+          Nodo currentNode = null;
+          if(i < middle ){
+               currentNode = this.cabeza; 
+               int currentIndex = this.longitud/2;
+               for( int j = 0; j <= middle, j++){
+                    if(j == i){
+                         return currentNode;
+                    }else{
+                         currentNode = currentNode.siguiente;
+                    }
+               }
+          }else{
+               currentNode = this.rabo; 
+               int currentIndex = this.longitud;
+               for( int j = this.longitud; j >= middle, j--){
+                    if(j == i){
+                         return currentNode;
+                    }else{
+                         currentNode = currentNode.anterior;
+                    }
+               }
+          }
+          return currentNode;
      }
 
      public String toString(){
